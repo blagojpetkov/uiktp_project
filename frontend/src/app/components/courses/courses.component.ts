15b5ee18dc3d84a7184c83977dadff16ebc57e74
MetoA <metoandov@gmail.com>
2023-05-28 23:17:13 +0200

    headers change and background change, fix courses style

--- frontend/src/app/components/courses/courses.component.ts
+++ frontend/src/app/components/courses/courses.component.ts
@@ -17,8 +17,6 @@ export class CoursesComponent implements OnInit {
   categories: string[] = ['Data Science', 'Web Development', 'Python', 'Drawing', 'Marketing', 'Music', 'Mobile Development']
   activeCategory = this.categories[0];
   coursesByCategory: Course[] = [];
-  course:any;
-  course2:any;
 
   constructor(
     private coursesService: CoursesService,
@@ -32,47 +30,7 @@ export class CoursesComponent implements OnInit {
     this.coursesService.getCourses().subscribe(mostPopularCourses => this.courses = mostPopularCourses.slice(0, 5))
     this.coursesService.getCoursesByCategory(this.activeCategory).subscribe(result => {
       this.coursesByCategory = result;
-      console.log(result);
-      
     })
-    this.setUpCourse();
-    
-  }
-
-  setUpCourse(){
-    debugger
-    this.course.name="Data Science Course"
-    // this.course.
-    this.course.category=this.categories[0];
-    this.course.description="Description for course";
-    const enrollmentTemp = new Enrollment;
-    enrollmentTemp.course = this.course;
-    enrollmentTemp.enrollmentDate=new Date(2023,5,19);
-    enrollmentTemp.id=0
-    enrollmentTemp.user=new User;
-    const enrollments = [];
-    enrollments.push(enrollmentTemp);
-    
-    this.course.image= new Blob();
-    this.course.enrollments=enrollments;
-    this.course2 = new Course();
-this.course2.name = "Data Science Course 2";
-this.course2.category = this.categories[0];
-this.course2.description = "Description for course 2";
-const enrollmentTemp2 = new Enrollment();
-enrollmentTemp.course = this.course2;
-enrollmentTemp.enrollmentDate = new Date(2023, 5, 19);
-enrollmentTemp.id = 0;
-enrollmentTemp.user = new User();
-const enrollments2 = [];
-enrollments.push(enrollmentTemp);
-
-this.course2.image = new Blob();
-this.course2.enrollments = enrollments;
-    this.courses.push(this.course2)
-    this.courses.push(this.course)
-    
-
   }
   onCategoryChange(category: string) {
     this.activeCategory = category;
